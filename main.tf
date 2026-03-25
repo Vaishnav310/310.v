0ada5896a649d7036fa5f61e90b7ea998a99c183
Vaishnav <jaishnav111@gmail.com>
2026-03-25 15:50:55 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    fortios = {
+      source  = "fortinetdev/fortios"
+      version = "1.24.1"
+    }
+  }
+}
+
+provider "fortios" {
+  hostname = "192.168.10.9"
+  username = "admin"
+  password = "Vaishnav.310"
+  insecure = true
+  vdom     = "root"
+}
+
+resource "fortios_firewall_address" "test1" {
+  name            = "terraform-test-ip"
+  subnet          = "192.168.99.10 255.255.255.255"
+  type            = "ipmask"
+  update_if_exist = true
+}
